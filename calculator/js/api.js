506a2e9f9f41a6e5e855588b7063d787ffb17cd7
mastlouis <mastlouis@wpi.edu>
2019-11-20 21:55:05 -0500

    Added some new constants and JS to test lambdas

---
+++ calculator/js/api.js
@@ -0,0 +1,8 @@
+// all access driven through BASE. Must end with a SLASH
+// be sure you change to accommodate your specific API Gateway entry point
+var base_url = "https://c9nze210f7.execute-api.us-east-2.amazonaws.com/Beta"; 
+
+var add_url    = base_url + "calculator";   // POST
+var create_url = base_url + "constant";     // POST
+var delete_url = base_url + "delete";       // Can't send JSON to DELETE request. This is POST
+var list_url   = base_url + "constants";    // GET
---
+++ calculator/js/create.js
@@ -0,0 +1,49 @@
+function processCreateResponse(result) {
+  // Can grab any DIV or SPAN HTML element and can then manipulate its
+  // contents dynamically via javascript
+  console.log("result:" + result);
+
+  refreshConstantsList();
+}
+
+function handleCreateClick(e) {
+  var form = document.createForm;
+ 
+  var data = {};
+  data["name"]               = form.constantName.value;
+  
+  if (form.system.checked) {  // be sure to flag system constant requests...
+     data["system"] = true;
+  }
+  
+  // base64EncodedValue":"data:text/plain;base64,My4xND....."
+  var segments = document.createForm.base64Encoding.value.split(',');
+  data["base64EncodedValue"] = segments[1];  // skip first one 
+
+  var js = JSON.stringify(data);
+  console.log("JS:" + js);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", create_url, true);
+
+  // send the collected data as JSON
+  xhr.send(js);
+
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+    console.log(xhr);
+    console.log(xhr.request);
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+    	 if (xhr.status == 200) {
+	      console.log ("XHR:" + xhr.responseText);
+	      processCreateResponse(xhr.responseText);
+    	 } else {
+    		 console.log("actual:" + xhr.responseText)
+			  var js = JSON.parse(xhr.responseText);
+			  var err = js["response"];
+			  alert (err);
+    	 }
+    } else {
+      processCreateResponse("N/A");
+    }
+  };
+}
