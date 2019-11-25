44e0a888e60c5c847250a584da5442e95e25d4cb
Grace Antedomenico <g.antedomenico@gmail.com>
2019-11-25 11:28:19 -0500

    spock wants what i have (and other minor changes)

--- webpages/scripts/api.js
+++ webpages/scripts/api.js
@@ -3,17 +3,17 @@
 var base_url = " https://8qmw7avi1m.execute-api.us-east-2.amazonaws.com/Beta/"; 
 //This is our calculator API. I'll put in the real one when we have it
 
-var upload_segment_url       = base_url + "upload-segment";      //POST
-var delete_segment_url       = base_url + "delete-segment";      //POST
-var list_segments_url        = base_url + "list-segments";       //GET
-var search_segments_url      = base_url + "search-segments";     //GET
-var create_playlist_url      = base_url + "create-playlist";     //POST
-var delete_playlist_url      = base_url + "delete-playlist";     //POST
-var list_playlists_url       = base_url + "list-playlists";      //POST
-var list_playlist_segments_url = base_url + "list-playlist-segments" //GET
-var remove_from_playlist_url = base_url + "remove-from-playlist";//POST
-var add_to_playlist_url      = base_url + "add-to-playlist";     //POST
-var mark_unmakr_local_url    = base_url + "mark-unmark-local";   //POST
-var register_site_url        = base_url + "register-site";       //POST
-var unregister_site_url      = base_url + "unregister-site";     //POST
-var list_sites_url           = base_url + "list-sites";          //GET
+var upload_segment_url         = base_url + "upload-segment";         //POST
+var delete_segment_url         = base_url + "delete-segment";         //POST
+var list_segments_url          = base_url + "list-segments";          //GET
+var search_segments_url        = base_url + "search-segments";        //GET
+var create_playlist_url        = base_url + "create-playlist";        //POST
+var delete_playlist_url        = base_url + "delete-playlist";        //POST
+var list_playlists_url         = base_url + "list-playlists";         //POST
+var list_playlist_segments_url = base_url + "list-playlist-segments"; //GET
+var remove_from_playlist_url   = base_url + "remove-from-playlist";   //POST
+var add_to_playlist_url        = base_url + "add-to-playlist";        //POST
+var mark_unmakr_local_url      = base_url + "mark-unmark-local";      //POST
+var register_site_url          = base_url + "register-site";          //POST
+var unregister_site_url        = base_url + "unregister-site";        //POST
+var list_sites_url             = base_url + "list-sites";             //GET
