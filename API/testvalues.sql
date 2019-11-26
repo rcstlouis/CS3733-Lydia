33266a6889727e8a55046e00a4b38ba483e07c46
mastlouis <mastlouis@wpi.edu>
2019-11-26 00:35:54 -0500

    Colors on stylesheet for readability

--- API/testvalues.sql
+++ API/testvalues.sql
@@ -5,7 +5,8 @@ select * from innodb.playlists;
 
 -- Segments
 -- Schema: (id, name, originFilePath, originSite, remotelyAvailable, character, sentence)
-delete from innodb.segments;
+delete from innodb.segments where remotelyAvailable = true;
+delete from innodb.segments where remotelyAvailable = false;
 insert into innodb.segments values ("3", "salt", "https://3733lydia.s3.us-east-2.amazonaws.com/segments/salt.ogg", "https://3733lydia.s3.us-east-2.amazonaws.com/segments/", true, "Crater", "It needs love as much as it needs salt.");
 insert into innodb.segments values ("4", "emotion", "https://3733lydia.s3.us-east-2.amazonaws.com/segments/emotion.ogg", "https://3733lydia.s3.us-east-2.amazonaws.com/segments/", true, "Spock", "Ah, yes. One of your Earth emotions.");
 insert into innodb.segments values ("5", "shower", "https://3733lydia.s3.us-east-2.amazonaws.com/segments/shower.ogg", "https://3733lydia.s3.us-east-2.amazonaws.com/segments/", false, "Tormolen", "One man was taking a shower fully clothed.");
