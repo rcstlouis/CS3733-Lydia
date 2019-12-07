d0ffa910ae734369ec955655b00aac739870cb40
maggiearaque <maraque@wpi.edu>
2019-12-06 22:54:33 -0500

    Add segment to playlist test case created

---
+++ LydiaAWS/src/test/java/com/amazonaws/lambda/demo/AddSegmentToPlaylistHandlerTest.java
@@ -0,0 +1,29 @@
+package com.amazonaws.lambda.demo;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.lambda.demo.http.AddSegmentToPlaylistRequest;
+import com.amazonaws.lambda.demo.http.AddSegmentToPlaylistResponse;
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class AddSegmentToPlaylistHandlerTest {
+	
+	@Test
+	public void testAddSegment() {
+		AddSegmentToPlaylistRequest req = new AddSegmentToPlaylistRequest("3", "test");
+		AddSegmentToPlaylistResponse res = new AddSegmentToPlaylistHandler().handleRequest(req, createContext("AddToPlaylist"));
+		
+		Assert.assertEquals(200, res.httpCode);
+	}
+	
+	Context createContext(String apiCall) {
+        TestContext2 ctx = new TestContext2();
+        ctx.setFunctionName(apiCall);
+        return ctx;
+    }
+
+}
