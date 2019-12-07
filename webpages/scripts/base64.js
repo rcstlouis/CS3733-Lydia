2148c363144902ec8a98afa1866adfb08837b293
Grace Antedomenico <g.antedomenico@gmail.com>
2019-12-06 23:57:03 -0500

    base64 works

--- webpages/scripts/base64.js
+++ webpages/scripts/base64.js
@@ -4,16 +4,16 @@ var reader = new FileReader();
 reader.readAsDataURL(file);
 
 reader.onload = function () {
-  document.createForm.base64Encoding.value = reader.result;
-  document.createForm.createButton.disabled = false;
+  document.uploadSegmentForm.base64Encoding.value = reader.result;
+  document.uploadSegmentForm.uploadSegmentButton.disabled = false;
 };
 }
 
 // When file is selected, stash base64 value in the encoding field.  
 function handleFileSelect(evt) {
   var files = evt.target.files; 
-  if (files[0].size > 250000) {  // make as large or small as you need
-    document.createForm.base64Encoding.value = "";
+  if (files[0].size > 1000000) {  // make as large or small as you need
+    document.uploadSegmentForm.base64Encoding.value = "";
     alert("File size too large to use:" + files[0].size + " bytes");
   } else {
     getBase64(files[0]); // request the load (async)
