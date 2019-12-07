b4a1b52528c83d2ff5a3d116b76703d92d56d18f
tsprowl <tsprowl25@hotmail.com>
2019-12-06 23:17:37 -0500

    Deleted search functions that are no longer needed

--- LydiaAWS/src/main/java/com/amazonaws/lambda/demo/db/SegmentsDAO.java
+++ LydiaAWS/src/main/java/com/amazonaws/lambda/demo/db/SegmentsDAO.java
@@ -43,63 +43,6 @@ public class SegmentsDAO {
         }
     }
 	
-	public List<Segment> searchByCharacter(String character) throws Exception{
-		List<Segment> allSegments = new ArrayList<>();
-        try {
-        	PreparedStatement ps = conn.prepareStatement("SELECT * FROM segments WHERE `character` RLIKE '.*\\\\?.*';");
-            ps.setString(1, character);
-            ResultSet resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-                Segment s = generateSegment(resultSet);
-                allSegments.add(s);
-            }
-            resultSet.close();
-            return allSegments;
-
-        } catch (Exception e) {
-            throw new Exception("Failed in searching segments: " + e.getMessage());
-        }
-	}
-	
-	public List<Segment> searchBySentence(String sentence) throws Exception{
-		List<Segment> allSegments = new ArrayList<>();
-        try {
-        	PreparedStatement ps = conn.prepareStatement("SELECT * FROM segments WHERE `sentence` RLIKE '.*\\\\?.*';");
-            ps.setString(1, sentence);
-            ResultSet resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-                Segment s = generateSegment(resultSet);
-                allSegments.add(s);
-            }
-            resultSet.close();
-            return allSegments;
-
-        } catch (Exception e) {
-            throw new Exception("Failed in searching segments: " + e.getMessage());
-        }
-	}
-	
-	public List<Segment> searchByAll(String search) throws Exception{
-		List<Segment> allSegments = new ArrayList<>();
-        try {
-        	PreparedStatement ps = conn.prepareStatement("SELECT * FROM segments WHERE `character` RLIKE '.*\\\\?.*' OR ;");
-            ps.setString(1, search);
-            ps.setString(2, search);
-            ResultSet resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-                Segment s = generateSegment(resultSet);
-                allSegments.add(s);
-            }
-            resultSet.close();
-            return allSegments;
-
-        } catch (Exception e) {
-            throw new Exception("Failed in searching segments: " + e.getMessage());
-        }
-	}
 	
 	public boolean addSegment(Segment segment) throws Exception {
         try {
