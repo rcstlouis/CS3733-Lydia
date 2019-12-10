81a2ec1fa94f4833a0487f6207977fa92fc35ea7
tsprowl <tsprowl25@hotmail.com>
2019-12-10 13:45:37 -0500

    53.7% coverage

--- LydiaAWS/src/test/java/com/amazonaws/lambda/demo/AddSegmentToPlaylistHandlerTest.java
+++ LydiaAWS/src/test/java/com/amazonaws/lambda/demo/AddSegmentToPlaylistHandlerTest.java
@@ -8,15 +8,20 @@ import org.junit.Test;
 
 import com.amazonaws.lambda.demo.http.AddSegmentToPlaylistRequest;
 import com.amazonaws.lambda.demo.http.AddSegmentToPlaylistResponse;
+import com.amazonaws.lambda.demo.http.RemoveSegmentFromPlaylistRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 
 public class AddSegmentToPlaylistHandlerTest {
 	@Test
 	public void testAddSegment() {
-		AddSegmentToPlaylistRequest req = new AddSegmentToPlaylistRequest("7", "test");
+		AddSegmentToPlaylistRequest req = new AddSegmentToPlaylistRequest("7", "test2: Electric Testaloo");
 		AddSegmentToPlaylistResponse res = new AddSegmentToPlaylistHandler().handleRequest(req, createContext("AddToPlaylist"));
 		
+		AddSegmentToPlaylistRequest req2 = new AddSegmentToPlaylistRequest();
+		AddSegmentToPlaylistResponse res2 = new AddSegmentToPlaylistResponse("Success!");
+
 		Assert.assertEquals(200, res.httpCode);
+		Assert.assertEquals(200, res2.httpCode);
 	}
 	
 	Context createContext(String apiCall) {
@@ -24,5 +29,11 @@ public class AddSegmentToPlaylistHandlerTest {
         ctx.setFunctionName(apiCall);
         return ctx;
     }
+	
+	/*@Test
+	public void testDeleteSegment() {
+		RemoveSegmentFromPlaylistRequest req = new RemoveSegmentFromPlaylistRequest();
+		RemoveSegmentFromPlaylistResponse rep = new RemoveSegmentFromPlaylistResponse()
+	}*/
 
 }
