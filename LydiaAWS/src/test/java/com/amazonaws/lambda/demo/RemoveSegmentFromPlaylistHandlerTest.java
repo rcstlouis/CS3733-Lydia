a5f303fee0836dae3f8886012c349f45a1e6af24
tsprowl <tsprowl25@hotmail.com>
2019-12-12 20:03:46 -0500

    14.2% coverage

--- LydiaAWS/src/test/java/com/amazonaws/lambda/demo/RemoveSegmentFromPlaylistHandlerTest.java
+++ LydiaAWS/src/test/java/com/amazonaws/lambda/demo/RemoveSegmentFromPlaylistHandlerTest.java
@@ -24,7 +24,19 @@ public class RemoveSegmentFromPlaylistHandlerTest {
 		RemoveSegmentFromPlaylistHandler handler = new RemoveSegmentFromPlaylistHandler();
 		RemoveSegmentFromPlaylistResponse res = handler.handleRequest(new RemoveSegmentFromPlaylistRequest("7", "test2: Electric Testaloo", 1), createContext("RemoveSegmentFromPlaylist"));
 		
+		RemoveSegmentFromPlaylistRequest req2 = new RemoveSegmentFromPlaylistRequest();
+		RemoveSegmentFromPlaylistResponse res2 = new RemoveSegmentFromPlaylistHandler().handleRequest(req2, createContext("RemoveSegmentFromPlaylistFail"));
+		
+		RemoveSegmentFromPlaylistRequest req3 = new RemoveSegmentFromPlaylistRequest();
+		req3.setSegmentName("5726");
+		req3.setPlaylistName("For legal reasons, this playlist name is a joke");
+		req3.setPlaylistEntryNum(123456);
+		
 		Assert.assertEquals(200,  res.statusCode);
+		Assert.assertEquals(422, res2.statusCode);
+		Assert.assertEquals("5726", req3.getSegmentName());
+		Assert.assertEquals("For legal reasons, this playlist name is a joke", req3.getPlaylistName());
+		Assert.assertEquals(123456, req3.getPlaylistEntryNum());
 	}
 
 }
